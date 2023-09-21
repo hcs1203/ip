1f31a1403195ac8af1a051bdf64991ceeae791bc
hcs1203 <solomonrajhannah@gmail.com>
2023-09-21 21:38:09 +0800

    A-Packages

---
+++ src/main/java/Duke/Task.java
@@ -0,0 +1,35 @@
+package Duke;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Task {
+    protected String name;
+    protected boolean isDone = false;
+    public Task(String name) {
+        this.name = name;
+    }
+    public void mark(){
+        isDone = true;
+    }
+    public void unmark(){
+        isDone = false;
+    }
+
+    public String printTask() {
+        return (isDone) ? "[X] " + name + "\n" : "[ ] " + name + "\n";
+    }
+
+    public void addToFile() {
+        try {
+            FileWriter Writer
+                    = new FileWriter("data/data.txt", true);
+            Writer.write(
+                    "  | 0 | " + name);
+            Writer.close();
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Duke/ToDos.java
@@ -0,0 +1,28 @@
+package Duke;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+class ToDos extends Task {
+    public ToDos(String name) {
+        super(name);
+    }
+
+    @Override
+    public String printTask() {
+        return (isDone) ? "[T] [X] " + name + "\n" : "[T] [ ] " + name + "\n";
+    }
+
+    @Override
+    public void addToFile() {
+        try {
+            FileWriter Writer
+                    = new FileWriter("data/data.txt", true);
+            Writer.write("T | 0 | " + name + "\n");
+            Writer.close();
+        } catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
